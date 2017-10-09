349a13ab4bf782e9d381075aba8cb41be7e78feb
hvrd <ha@hvrd.com>
2017-10-09 16:10:13 +0200

    Basic setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const babel = require('gulp-babel');
 // Gulp deps
 
 gulp.task('default', function(){
     // Gulp tasks
+    gulp.src(["es6/**/*.js","public/es6/**/*.js"])
+    .pipe(eslint()).pipe(eslint.format());
+
+    // Node source 
+    gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist")); 
+    
+    // browser source 
+    gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
 
