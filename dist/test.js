535654c60f7eb240603c9ec4f2f744ed8c4fa095
ha <ha@hvrd.com>
2017-10-18 18:56:04 +0200

    Book progress

--- dist/test.js
+++ dist/test.js
@@ -1,3 +1,5 @@
+
+
 "use strict";
 
 var sentences = [{ subject: "JavaScript", verb: "is", object: "great" }, { subject: "Elephants", verb: "are", object: "large" }];
@@ -55,4 +57,66 @@ var håvard = { name: "Håvard" };
 function greet(age, address) {
     console.log("I am " + this.name + " aged " + age + " at " + address);
 }
-greet.call(håvard, 47);
+greet.call(håvard, 47, "Høgåsveien 17A");
+greet.apply(håvard, [47, "Høgåsveien 17A"]);
+
+var printHåvard = greet.bind(håvard);
+
+printHåvard(47, "Fortsatt Høgåsveien 17");
+
+var globalFunc = void 0;
+{
+    var blockVar = 'a';
+    globalFunc = function globalFunc() {
+        console.log(blockVar);
+    };
+}
+
+globalFunc();
+
+(function () {
+    console.log("IIFE - immediately invoked function expression");
+})();
+
+var arr = [1, 2, 3];
+var arr2 = arr.concat(4, 5, 6);
+
+console.log(arr2);
+
+var arr3 = [0, 1, 2, 3, 4, 5, 6];
+
+console.log(arr3.slice(3));
+
+var a4 = [{ name: "Qi", income: 9 }, { name: "HAL", income: 17 }, { name: "HA", income: 7 }];
+a4.sort(function (a, b) {
+    return a.income > b.income;
+});
+console.log(a4);
+
+console.log(a4.map(function (a) {
+    return a.name;
+}));
+
+var f = function f(a) {
+    return a.income;
+};
+console.log(a4.map(f));
+
+var a5 = [10, 11, 12];
+var a6 = [90, 91, 92];
+console.log(a5.map(function (a, i) {
+    return a + a6[i];
+}));
+
+var cards = [];
+var _arr = ['H', 'C', 'D', 'S'];
+for (var _i = 0; _i < _arr.length; _i++) {
+    var suit = _arr[_i];
+    for (var value = 1; value <= 13; value++) {
+        cards.push({ suit: suit, value: value });
+    }
+}
+
+console.log(cards.filter(function (a) {
+    return a.suit === 'H';
+}));
--- es6/test.js
+++ es6/test.js
@@ -1,3 +1,5 @@
+
+
 "use strict";
 
 const sentences=[
@@ -37,4 +39,62 @@ greet.apply(håvard, [47,"Høgåsveien 17A"]);
 const printHåvard = greet.bind(håvard);
 
 printHåvard(47,"Fortsatt Høgåsveien 17");
+
+let globalFunc;
+{
+    let blockVar='a';
+    globalFunc=function()
+    {
+        console.log(blockVar);
+    };
+}
+
+globalFunc();
+
+(function(){
+    console.log("IIFE - immediately invoked function expression");
+})();
+
+const arr = [1,2,3];
+const arr2=arr.concat(4,5,6);
+
+console.log(arr2);
+
+const arr3= [0,1,2,3,4,5,6];
+
+console.log(arr3.slice(3));
+
+const a4=[{name:"Qi", income:9},{name:"HAL", income:17},{name:"HA", income:7} ];
+a4.sort((a,b)=>a.income>b.income);
+console.log(a4);
+
+console.log(a4.map(a=>a.name));
+
+const f=function(a){return a.income;};
+console.log(a4.map(f));
+
+const a5=[10,11,12];
+const a6=[90,91,92];
+console.log(a5.map((a,i)=>a+a6[i]));
+
+const cards = [];
+for(let suit of ['H','C','D','S'])
+{
+    for(let value=1;value<=13;value++)
+        cards.push({suit,value});
+}
+
+console.log(cards.filter(a=>a.suit=='H'));
+
+const a7=[10,11,12,13];
+const sum=a7.reduce((a,x)=>a+=x,0);
+console.log("a7.reduce((a,x)=>a+=x,0) : ",sum);
+
+const words=["Baseball", "Rodeo", "Angel", "Papya", "Uniform", "Joker", "Clover", "Bali", "Jekyll", "Unfit"];
+const grouped=words.reduce((a,x)=>{
+    if(!a[x[0]]) a[x[0]]=[];
+    a[x[0]].push(x);
+    return a;},{});
+console.log("words.reduce : ",grouped);
+
 
